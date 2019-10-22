901655fb849cb93794dd8c4eec4deba6d58e77f4
JoshuaGutie <Josh.Gvtie@gmail.com>
2019-10-22 02:58:48 -0500

    hack

---
+++ 06week/hackathon.js
@@ -0,0 +1,23 @@
+var standard_input = process.stdin;
+
+// Set input character encoding.
+standard_input.setEncoding("utf-8");
+
+// Prompt user to input data in console.
+console.log("Please guess a letter:");
+
+// When user input data and click enter key.
+
+standard_input.on("data", function(guess) {
+  let arr = "hello";
+  var splitWord = arr.split("");
+  console.log(splitWord);
+
+  let dashes = splitWord.map(dashes => {
+    return "_";
+  });
+  console.log(dashes);
+  var position = splitWord.indexOf(guess);
+  console.log(position);
+  console.log(guess);
+});
