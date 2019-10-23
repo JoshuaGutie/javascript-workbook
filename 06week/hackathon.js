bb1d375b71710ede327594c1342ed14248d934ac
JoshuaGutie <Josh.Gvtie@gmail.com>
2019-10-23 10:57:24 -0500

    hack

--- 06week/hackathon.js
+++ 06week/hackathon.js
@@ -11,13 +11,23 @@ console.log("Please guess a letter:");
 standard_input.on("data", function(guess) {
   let arr = "hello";
   var splitWord = arr.split("");
-  console.log(splitWord);
+  //console.log(splitWord);
+  var cleanGuess = guess.trim().toLowerCase();
 
   let dashes = splitWord.map(dashes => {
     return "_";
   });
-  console.log(dashes);
-  var position = splitWord.indexOf(guess);
+  console.log("guess this word:", dashes);
+  var incorrectGuess = [];
+  var position = splitWord.indexOf(cleanGuess);
+  if (position > -1) {
+    dashes[position] = cleanGuess;
+  } else {
+    incorrectGuess.push(cleanGuess);
+  }
+  var correctGuesses = dashes;
+  console.log("correct Guesses:", correctGuesses);
+  console.log("incorrect guess:", incorrectGuess);
   console.log(position);
-  console.log(guess);
+  console.log(cleanGuess);
 });
