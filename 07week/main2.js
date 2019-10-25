b9f0715adf377a3b08e5c5b0c01beb0b1273a1fc
JoshuaGutie <Josh.Gvtie@gmail.com>
2019-10-24 20:59:38 -0500

    random

---
+++ 07week/main2.js
@@ -0,0 +1,40 @@
+window.onload = function() {
+  console.log("hello world");
+  populateUsers();
+};
+
+let populateUsers = function() {
+  fetch("https://randomuser.me/api?results=10")
+    .then(function(res) {
+      console.log("processing results of api fetch call", res);
+      return res.json();
+    })
+    .then(function(payload) {
+      console.log(payload);
+      console.log("list of users", payload.results);
+      payload.results.forEach(function(user, index) {
+        let li = document.createElement("li");
+        let img = document.createElement("img");
+        let span = document.createElement("span");
+        let ul = document.getElementById("listOfUsers");
+        img.setAttribute("src", user.picture.large);
+        span.innerText = `${user.name.title} ${user.name.first} ${user.name.last}`;
+        let button = document.createElement("button");
+        li.appendChild(img);
+        li.appendChild(span);
+        li.appendChild(button);
+        button.innerText = "show my email";
+        button.setAttribute("data-email", user.email);
+        button.addEventListener("click", function(event) {
+          var clickedButton = event.target;
+          var email = clickedButton.getAttribute("data-email");
+          var clickedLi = clickedButton.parentElement;
+          let emailSpan = document.createElement("span");
+          emailSpan.innerText = email;
+          clickedLi.appendChild(email.span);
+          clickedLi.removeChild(clickedButton);
+        });
+        ul.appendChild(li);
+      });
+    });
+};
